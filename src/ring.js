4260b71de1683354bc6f51b0e5b3423b8fcb3ae3
Simon Schlaetker <s.schlaetker@gmx.net>
2018-02-22 17:28:59 +0100

    'error'

--- src/ring.js
+++ src/ring.js
@@ -3,6 +3,9 @@ const api = require('./api/HueApi');
 const AWAITTIME = 1200;
 const AWAITTIMELONG = AWAITTIME;
 
+let count = 0;
+let resetTimeOut = null;
+
 function sleep(time) {
     return new Promise(function (resolve) {
         setTimeout(resolve, time);
@@ -24,6 +27,18 @@ async function offOn() {
 
 
 async function ringDingDong() {
+
+    count++;
+
+    if (count < 5) {
+        if (!resetTimeOut)
+            return;
+        resetTimeOut = setTimeout(function () {
+            count = 0;
+
+        }, 1000)
+    }
+
 
     console.log('its donging..');
     const lights = await api.getLights();
